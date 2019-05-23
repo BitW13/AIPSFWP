65618f739590e03a48d42b7aff0a712b7bb607f7
Roman Kurpachenko <Roman_Kurpachenko@epam.com>
2019-05-23 18:08:36 +0300

    I'm a moron

---
+++ AIPSFWP.WebApi/AIPSFWP.WebApi/Controllers/WorkTaskController.cs
@@ -0,0 +1,106 @@
+﻿using AIPSFWP.BLL.BusinessLogic;
+using AIPSFWP.Common.Entities.WorkTasks;
+using AIPSFWP.WebApi.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AIPSFWP.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkTaskController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IBusinessLogic db;
+
+        public WorkTaskController(IBusinessLogic db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<IndexEditWorkTaskViewModel>> Get()
+        {
+            var workTasks = await db.WorkTasks.GetAllAsync();
+
+            var models = new List<IndexEditWorkTaskViewModel>();
+
+            foreach (var v in workTasks)
+            {
+                var workTask = await db.WorkTasks.GetItemByIdAsync(v.Id);
+
+                models.Add(mapper.Map<IndexEditWorkTaskViewModel>(v));
+            }
+
+            return models;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var workTask = await db.WorkTasks.GetItemByIdAsync(id);
+
+            if (workTask == null)
+            {
+                return NotFound();
+            }
+
+            var model = mapper.Map<IndexEditWorkTaskViewModel>(workTask);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateWorkTaskViewModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            var workTask = await db.WorkTasks.CreateAsync(mapper.Map<WorkTask>(model));
+
+            model = mapper.Map<CreateWorkTaskViewModel>(workTask);
+
+            return Ok(model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, IndexEditEmployeeViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            await db.WorkTasks.UpdateAsync(mapper.Map<WorkTask>(model));
+
+            return Ok(model);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var workTask = await db.WorkTasks.GetItemByIdAsync(id);
+
+            if (workTask == null)
+            {
+                return BadRequest();
+            }
+
+            await db.Employees.DeleteAsync(workTask.Id);
+
+            var model = mapper.Map<IndexEditWorkTaskViewModel>(workTask);
+
+            return Ok(model);
+        }
+    }
+}
