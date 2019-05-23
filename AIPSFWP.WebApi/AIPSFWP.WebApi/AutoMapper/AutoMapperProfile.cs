cd32eafe2f6822ec2833fdc70b57c405db49173c
Roman Kurpachenko <Roman_Kurpachenko@epam.com>
2019-05-23 18:02:36 +0300

    Add api-controller for WorkTask

--- AIPSFWP.WebApi/AIPSFWP.WebApi/AutoMapper/AutoMapperProfile.cs
+++ AIPSFWP.WebApi/AIPSFWP.WebApi/AutoMapper/AutoMapperProfile.cs
@@ -1,10 +1,7 @@
 ﻿using AIPSFWP.Common.Entities.Employees;
+using AIPSFWP.Common.Entities.WorkTasks;
 using AIPSFWP.WebApi.ViewModels;
 using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AIPSFWP.WebApi.AutoMapper
 {
@@ -19,6 +16,10 @@ namespace AIPSFWP.WebApi.AutoMapper
             CreateMap<Employee, IndexEditEmployeeViewModel>()
                 .ReverseMap();
             CreateMap<EmployeeData, IndexEditEmployeeViewModel>()
+                .ReverseMap();
+            CreateMap<WorkTask, IndexEditWorkTaskViewModel>()
+                .ReverseMap();
+            CreateMap<WorkTask, CreateWorkTaskViewModel>()
                 .ReverseMap();
         }
     }
